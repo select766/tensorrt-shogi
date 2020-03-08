3a18c4bf92e213a92b26e6b0808b8515442a54b0
select766 <select766@outlook.jp>
2020-03-08 17:21:02 +0900

    makefile

--- cpp/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-TENSORRT_DIR=../../TensorRT-7.0.0.11
-g++ -Wall -Wno-deprecated-declarations -std=c++11  -I"../common" -I"/usr/local/cuda/include" -I"/usr/local/cuda/include" -I"${TENSORRT_DIR}/include" -D_REENTRANT -c -o shogi_resnet.o shogi_resnet.cpp
-g++ -Wall -Wno-deprecated-declarations -std=c++11  -I"../common" -I"/usr/local/cuda/include" -I"/usr/local/cuda/include" -I"${TENSORRT_DIR}/include" -D_REENTRANT -c -o logger.o logger.cpp
-g++ -o ./shogi_resnet ./shogi_resnet.o ./logger.o -L"/usr/local/cuda/lib64" -L"/usr/local/cuda/lib64" -L"${TENSORRT_DIR}/lib" -Wl,--start-group -lnvinfer -lnvparsers -lnvinfer_plugin -lnvonnxparser -lcudnn -lcublas -lcudart -lrt -ldl -lpthread -lmyelin -lnvrtc -Wl,--end-group
